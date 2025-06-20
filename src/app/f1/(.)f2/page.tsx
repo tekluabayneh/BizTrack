e095ffc73003292daffee5d147d9cba80141c080
teklu abayneh <tekluabayneh@gmail.com>
2025-06-20 23:04:00 +0300

    Add src/app/f1 directory and files

---
+++ src/app/f1/(.)f2/page.tsx
@@ -0,0 +1,3 @@
+export default function ComplexDashboardPage() {
+  return <h1>this is the intercepting page page f2 </h1>;
+}
---
+++ src/app/f1/f2/page.tsx
@@ -0,0 +1,8 @@
+export default function ComplexDashboardPage() {
+  return (
+    <>
+      <h1> f2 page</h1>
+      <br />
+    </>
+  );
+}
