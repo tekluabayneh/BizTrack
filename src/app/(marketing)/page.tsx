c3bbe01b1beed8bc96aa590865c3a05fcdd1a134
teklu abayneh <tekluabayneh@gmail.com>
2025-06-22 17:14:22 +0300

    Update marketing main page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
    <h1 className="text-4xl bg-red-500  grid grid-cols-1 text-blue-500 ">
-hello man it work though
+hello man it work though just
 
 			</h1>
 
