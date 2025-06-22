e2d8b32d13858c55d36c3c61f544b371dc42e3db
teklu abayneh <tekluabayneh@gmail.com>
2025-06-22 17:12:46 +0300

    Add data.ts file

---
+++ src/app/data.ts
@@ -0,0 +1,9 @@
+// data/users.js
+const users = [
+    { id: 1, name: "Teklu Abayneh", email: "teklu@example.com" },
+    { id: 2, name: "Sara Solomon", email: "sara@example.com" },
+    { id: 3, name: "Amanuel Tadesse", email: "amanuel@example.com" },
+];
+
+export default users;
+
