f2c90969c75728087b8f173e5c756fad9c984c10
teklu abayneh <tekluabayneh@gmail.com>
2025-06-22 17:14:22 +0300

    Update marketing revenue page

--- src/app/(marketing)/revenue/page.tsx
+++ src/app/(marketing)/revenue/page.tsx
@@ -1,15 +1,28 @@
 
+"use client"
+import axios from "axios"
+import { useEffect } from "react"
 
-const customer = () =>{
+const customer = () => {
+    useEffect(() => {
+        const FetchData = async () => {
+            const res = await axios("http://localhost:3000/components", { method: "post" })
+            return res.data
+        }
+        FetchData()
 
-return (
-<div> 
 
-<h1>this the login</h1>
+    }, [])
 
-		</div>
+    return (
 
-)
+        <div>
+
+            <h1>this the login</h1>
+
+        </div>
+
+    )
 
 }
 export default customer 
